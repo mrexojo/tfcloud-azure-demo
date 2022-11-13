6a769285f473a52ff4193b27fb4ca9ec3f085f34
mrexojo ☁️ <mrexojo@gmail.com>
2022-11-13 16:46:06 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "clientid" {
+    type = string
+}
+variable "clientsecret" {
+    type = string
+}
+variable "subscriptionid" {
+    type = string
+}
+variable "tenantid" {
+    type = string
+}
