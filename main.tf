b30f80de324fc4f7ee2c25c5cad6e66bf73150e9
mrexojo ☁️ <mrexojo@gmail.com>
2022-11-13 12:49:49 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+# Provider version
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.30.0"
+    }
+  }
+}
+# Provideer declaration
+provider "azurerm" {
+  features{}
+}
+
+#--------------------------------------
+# Azure Resource Group
+resource "azurerm_resource_group" "rg" {
+  name = "rgcurso"
+  location = "francecentral"
+}
+
+# Azure Storage Account 
+resource "azurerm_storage_account" "sa" {
+  name                     = "sacurso"
+  resource_group_name      = "rgcurso" 
+  location                 = "francecentral"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
