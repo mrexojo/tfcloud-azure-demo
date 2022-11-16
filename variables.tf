5f44c64e7526b73196f2403391483fa03ad196c6
mrexojo ☁️ <mrexojo@gmail.com>
2022-11-16 10:47:11 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,12 @@
 variable "ARM_CLIENT_ID" {
+  default = "prueba"
 }
 variable "ARM_CLIENT_SECRET" {
+    default = "prueba"
 }
 variable "ARM_SUBSCRIPTION_ID" {
+    default = "prueba"
 }
 variable "ARM_TENANT_ID" {
+    default = "prueba"
 }
