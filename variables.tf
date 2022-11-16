c084f4f96a7f43caf1dce7edeb7fbda9ec57a983
mrexojo ☁️ <mrexojo@gmail.com>
2022-11-16 10:32:23 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,8 @@
-#variable "ARM_CLIENT_ID" {
-#    type = string
-#}
-#variable "ARM_CLIENT_SECRET" {
-#    type = string
-#}
-#variable "ARM_SUBSCRIPTION_ID" {
-#    type = string
-#}
-#variable "ARM_TENANT_ID" {
-#    type = string
-#}
+variable "ARM_CLIENT_ID" {
+}
+variable "ARM_CLIENT_SECRET" {
+}
+variable "ARM_SUBSCRIPTION_ID" {
+}
+variable "ARM_TENANT_ID" {
+}
