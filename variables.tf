0d9c4a0e990063e78543498b385a43687a20851e
mrexojo ☁️ <mrexojo@gmail.com>
2022-11-16 10:10:53 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,16 @@
 variable "ARM_CLIENT_ID" {
     type = string
+    default = "replace_me"
 }
 variable "ARM_CLIENT_SECRET" {
     type = string
+    default = "replace_me"
 }
 variable "ARM_SUBSCRIPTION_ID" {
     type = string
+    default = "replace_me"
 }
 variable "ARM_TENANT_ID" {
     type = string
+    default = "replace_me"
 }
